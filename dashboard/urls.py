413c8dd791796783c67e9a8a32afdaba419ab029
Derakhshan <github2reza@gmail.com>
2025-08-14 22:40:47 +0330

    feat(admin-skills): developed 4 function for skill crud operation

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
+    path("skills/", views.skill_page, name="skills"),
+    path("skills/create/", views.skill_create_page, name="skill-create"),
+    path("skills/edit/<int:skill_id>/", views.skill_edit_page, name="skill-edit"),
+    path("skills/delete/<int:skill_id>/", views.skill_delete_page, name="skill-delete"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
+from app.models import *
 
 # Create your views here.
+
 
 @login_required(login_url="dashboard:login")
 def index(request):
@@ -23,6 +25,55 @@ def login(request):
 
     return render(request, "admin/login.html")
 
+
+@login_required(login_url="dashboard:login")
 def logout(request):
     auth.logout(request)
     return redirect("dashboard:login")
+
+
+@login_required(login_url="dashboard:login")
+def skill_page(request):
+    skill = Skills.objects.all()
+    return render(request, "admin/pages/skill.html", {"skills": skill})
+
+
+@login_required(login_url="dashboard:login")
+def skill_create_page(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        percentage = request.POST.get("percentage")
+        if title and percentage:
+            skill = Skills(title=title, percentage=percentage)
+            skill.save()
+            messages.success(request, "Skill created successfully")
+            return redirect("dashboard:skills")
+        else:
+            messages.error(request, "All fields are required")
+            return redirect("dashboard:skill-create")
+
+
+@login_required(login_url="dashboard:login")
+def skill_edit_page(request, skill_id):
+    skill = Skills.objects.get(id=skill_id)
+    if request.method == "POST":
+        title = request.POST.get("title")
+        percentage = request.POST.get("percentage")
+        if title and percentage:
+            skill.title = title
+            skill.percentage = percentage
+            skill.save()
+            messages.success(request, "Skill updated successfully")
+            return redirect("dashboard:skills")
+        else:
+            messages.error(request, "All fields are required")
+            return redirect("dashboard:skills", skill_id=skill_id)
+
+
+@login_required(login_url="dashboard:login")
+def skill_delete_page(request, skill_id):
+    skill = Skills.objects.get(id=skill_id)
+    if request.method == "POST":
+        skill.delete()
+        messages.success(request, "Skill deleted successfully")
+        return redirect("dashboard:skills")
